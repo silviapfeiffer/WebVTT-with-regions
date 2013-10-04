a5b8be0197cd75be28f369fccf0165755cb1cbb0
Silvia Pfeiffer <silviapfeiffer1@gmail.com>
2013-10-04 23:22:24 +1000

    Move to using tests with expected outcomes, so we can test expected errors, too.

--- validate.js
+++ validate.js
@@ -4,38 +4,21 @@ ps = require('./parser.js');
 "use strict";
 var dir = "./tests/";
 
-function printErrors(errors, duration) {
-  var errorsLength = errors.length;
-
-  if (errorsLength > 0) {
-    for (var i = 0; i < errorsLength; i++) {
-      var error = errors[i],
-      message = "Line " + error.line,
-      column = error.col;
-      if (column) {
-        message += ", column " + column;
-      }
-      console.log(message + ": " + error.message);
-    }
-  }
-  console.log("Validated in " + duration + "ms.");
-  console.log("---");
-};
-
-function validate(data) {
+function validate(data, file) {
   var parser, parsedData;
 
   // parse textarea
   parser = ps.WebVTTParser();
   parsedData = parser.parse(data, 'captions');
 
-  // print errors and WebVTT file as parsed
-  if (parsedData.errors.length > 0) {
-    console.log(data);
-  } else {
-    console.log("!! VALID !!")
-  }
-  printErrors(parsedData.errors, parsedData.time);
+  fs.readFile(dir+file+".txt", 'utf8', function (err, content) {
+    if (err) {
+      return console.log(err);
+    }
+    if (content !== JSON.stringify(parsedData.errors)) {
+      console.log("Parse error for file " + file + ".vtt");
+    }
+  });
 }
 
 fs.readdir(dir, function(err, files) {
@@ -43,13 +26,15 @@ fs.readdir(dir, function(err, files) {
     return console.log(err);
   }
   files.forEach(function(file) {
-    fs.readFile(dir+file, 'utf8', function (err,data) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(dir+file);
-      validate(data);
-    });
+    // validate all vtt files in tests directory
+    if (file.split('.').pop() === 'vtt') {
+      fs.readFile(dir+file, 'utf8', function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        validate(data, file.split('.').shift());
+      });
+    }
   });
 });
 
